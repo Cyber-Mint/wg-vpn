7066f4d9dae0f861556183dd399010f1fd45e481
jack <jackdek11@gmail.com>
2023-07-24 21:37:58 +0200

    Added save_tunnels function to save tunnels changes to tunnels_file

--- backend/src/templates/register.sh
+++ backend/src/templates/register.sh
@@ -38,8 +38,13 @@ initialize_tunnels() {
     # Read the contents of the tunnels_file into the list
     if [ -f "tunnels_file" ]; then
         # If the file exists, read its contents into the list
-        tunnel_list=$(cat "tunnels_file")
+        tunnels=$(cat "tunnels_file")
     fi
+}
+
+save_tunnels() {
+    # Save the content of the tunnels into the tunnels_file
+    printf "%s" "$tunnels" > "$tunnels_file"
 }
 
 connect() {
